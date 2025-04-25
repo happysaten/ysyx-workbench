eaaa23e75d425b05cdee3c145dd46ba171bcdbf4
tracer-ysyx <tracer@ysyx.org>
2025-04-25 16:36:53 +0800

    > compile NEMU ysyx_22040000 李丹青 Linux DESKTOP-BOOBDOU 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 GNU/Linux  16:36:53 up  1:27,  1 user,  load average: 0.10, 0.06, 0.03

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -1,125 +1,261 @@
 /***************************************************************************************
-* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include <isa.h>
 
-/* We use the POSIX regex functions to process regular expressions.
- * Type 'man regex' for more information about POSIX regex functions.
+/* 我们使用POSIX正则表达式函数来处理正则表达式。
+ * 输入'man regex'以获取有关POSIX正则表达式函数的更多信息。
  */
 #include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ,
-
-  /* TODO: Add more token types */
-
+    TK_NOTYPE = 256, // 无类型
+    TK_EQ,           // 等于
+    TK_NUM,          // 数字
+    TK_HEX,          // 十六进制
+    TK_REG,          // 寄存器
+    TK_NEG,          // 负号
+    /* TODO: 添加更多的token类型 */
 };
 
 static struct rule {
-  const char *regex;
-  int token_type;
+    const char *regex; // 正则表达式
+    int token_type;    // token类型
 } rules[] = {
-
-  /* TODO: Add more rules.
-   * Pay attention to the precedence level of different rules.
-   */
-
-  {" +", TK_NOTYPE},    // spaces
-  {"\\+", '+'},         // plus
-  {"==", TK_EQ},        // equal
+    {" +", TK_NOTYPE},                   // 空格
+    {"\\+", '+'},                        // 加号
+    {"-", '-'},                          // 减号
+    {"\\*", '*'},                        // 乘号
+    {"/", '/'},                          // 除号
+    {"\\(", '('},                        // 左括号
+    {"\\)", ')'},                        // 右括号
+    {"0[xX][0-9a-fA-F]+", TK_HEX},       // 十六进制数字
+    {"[0-9]+", TK_NUM},                  // 十进制数字
+    {"\\$[a-zA-Z][a-zA-Z0-9]*", TK_REG}, // 寄存器
+    {"==", TK_EQ},                       // 等于
 };
 
 #define NR_REGEX ARRLEN(rules)
 
 static regex_t re[NR_REGEX] = {};
 
-/* Rules are used for many times.
- * Therefore we compile them only once before any usage.
+/* 规则会被多次使用。
+ * 因此我们在第一次使用前只编译一次。
  */
 void init_regex() {
-  int i;
-  char error_msg[128];
-  int ret;
+    int i;
+    char error_msg[128];
+    int ret;
 
-  for (i = 0; i < NR_REGEX; i ++) {
-    ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
-    if (ret != 0) {
-      regerror(ret, &re[i], error_msg, 128);
-      panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
+    for (i = 0; i < NR_REGEX; i++) {
+        ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
+        if (ret != 0) {
+            regerror(ret, &re[i], error_msg, 128);
+            panic("regex compilation failed: %s\n%s", error_msg,
+                  rules[i].regex);
+        }
     }
-  }
 }
 
 typedef struct token {
-  int type;
-  char str[32];
+    int type;     // token类型
+    char str[32]; // token字符串
 } Token;
 
-static Token tokens[32] __attribute__((used)) = {};
-static int nr_token __attribute__((used))  = 0;
+static Token tokens[32] __attribute__((used)) = {}; // 存储token的数组
+static int nr_token __attribute__((used)) = 0;      // token数量
 
+/* 解析表达式并生成token */
 static bool make_token(char *e) {
-  int position = 0;
-  int i;
-  regmatch_t pmatch;
+    int position = 0;
+    int i;
+    regmatch_t pmatch;
 
-  nr_token = 0;
+    nr_token = 0;
 
-  while (e[position] != '\0') {
-    /* Try all rules one by one. */
-    for (i = 0; i < NR_REGEX; i ++) {
-      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
-        char *substr_start = e + position;
-        int substr_len = pmatch.rm_eo;
+    while (e[position] != '\0') {
+        /* 逐个尝试所有规则 */
+        for (i = 0; i < NR_REGEX; i++) {
+            if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 &&
+                pmatch.rm_so == 0) {
+                char *substr_start = e + position;
+                int substr_len = pmatch.rm_eo;
 
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start);
+                // 日志记录匹配
+                Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+                    i, rules[i].regex, position, substr_len, substr_len,
+                    substr_start);
 
-        position += substr_len;
+                position += substr_len;
 
-        /* TODO: Now a new token is recognized with rules[i]. Add codes
-         * to record the token in the array `tokens'. For certain types
-         * of tokens, some extra actions should be performed.
-         */
+                /* 记录token */
+                if (rules[i].token_type != TK_NOTYPE) { // 忽略空格
+                    tokens[nr_token].type = rules[i].token_type;
+                    if (substr_len < sizeof(tokens[nr_token].str)) {
+                        strncpy(tokens[nr_token].str, substr_start, substr_len);
+                        tokens[nr_token].str[substr_len] =
+                            '\0'; // 字符串以空字符结尾
+                    } else {
+                        printf("Token too long at position %d\n", position);
+                        return false;
+                    }
 
-        switch (rules[i].token_type) {
-          default: TODO();
+                    // 处理负号
+                    if (tokens[nr_token].type == '-' &&
+                        (nr_token == 0 || tokens[nr_token - 1].type == '(' ||
+                         tokens[nr_token - 1].type == '+' ||
+                         tokens[nr_token - 1].type == '-' ||
+                         tokens[nr_token - 1].type == '*' ||
+                         tokens[nr_token - 1].type == '/' ||
+                         tokens[nr_token - 1].type == TK_EQ)) {
+                        tokens[nr_token].type = TK_NEG; // 将减号解释为负号
+                    }
+
+                    nr_token++;
+                }
+
+                break;
+            }
         }
 
-        break;
-      }
+        if (i == NR_REGEX) {
+            printf("no match at position %d\n%s\n%*.s^\n", position, e,
+                   position, "");
+            return false;
+        }
     }
 
-    if (i == NR_REGEX) {
-      printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
-      return false;
-    }
-  }
-
-  return true;
+    return true;
 }
 
+/* 检查表达式是否被一对匹配的括号包围 */
+static bool check_parentheses(int p, int q) {
+    if (tokens[p].type != '(' || tokens[q].type != ')') {
+        return false;
+    }
+    int balance = 0;
+    for (int i = p; i <= q; i++) {
+        if (tokens[i].type == '(')
+            balance++;
+        else if (tokens[i].type == ')')
+            balance--;
+        if (balance < 0)
+            return false; // 括号不匹配
+    }
+    return balance == 0;
+}
 
+/* 在表达式中寻找主运算符 */
+static int find_main_operator(int p, int q) {
+    int op = -1;
+    int min_priority = 100; // 假设优先级最大为100
+    int balance = 0;
+
+    for (int i = p; i <= q; i++) {
+        if (tokens[i].type == '(')
+            balance++;
+        else if (tokens[i].type == ')')
+            balance--;
+        else if (balance == 0) { // 不在括号内
+            int priority;
+            switch (tokens[i].type) {
+            case '+':
+            case '-':
+                priority = 1;
+                break;
+            case '*':
+            case '/':
+                priority = 2;
+                break;
+            default:
+                continue;
+            }
+            if (priority <= min_priority) {
+                min_priority = priority;
+                op = i;
+            }
+        }
+    }
+    return op;
+}
+
+/* 递归求值函数 */
+static word_t eval(int p, int q, bool *success) {
+    if (p > q) {
+        *success = false; // 表达式非法
+        return 0;
+    } else if (p == q) {
+        // 单个token，必须是数字或十六进制
+        if (tokens[p].type == TK_NUM) {
+            return strtoul(tokens[p].str, NULL, 10);
+        } else if (tokens[p].type == TK_HEX) {
+            return strtoul(tokens[p].str, NULL, 16);
+        } else {
+            *success = false;
+            return 0;
+        }
+    } else if (check_parentheses(p, q)) {
+        // 去掉括号后递归求值
+        return eval(p + 1, q - 1, success);
+    } else {
+        int op = find_main_operator(p, q);
+        if (op == -1) {
+            *success = false;
+            return 0;
+        }
+
+        if (tokens[op].type == TK_NEG) {
+            // 处理负号
+            word_t val = eval(op + 1, q, success);
+            if (!*success)
+                return 0;
+            return -val;
+        }
+
+        word_t val1 = eval(p, op - 1, success);
+        if (!*success)
+            return 0;
+        word_t val2 = eval(op + 1, q, success);
+        if (!*success)
+            return 0;
+
+        switch (tokens[op].type) {
+        case '+':
+            return val1 + val2;
+        case '-':
+            return val1 - val2;
+        case '*':
+            return val1 * val2;
+        case '/':
+            if (val2 == 0) {
+                *success = false; // 除零错误
+                return 0;
+            }
+            return val1 / val2;
+        default:
+            *success = false;
+            return 0;
+        }
+    }
+}
+
+/* 表达式求值接口 */
 word_t expr(char *e, bool *success) {
-  if (!make_token(e)) {
-    *success = false;
-    return 0;
-  }
-
-  /* TODO: Insert codes to evaluate the expression. */
-  TODO();
-
-  return 0;
+    if (!make_token(e)) {
+        *success = false;
+        return 0;
+    }
+    return eval(0, nr_token - 1, success);
 }
