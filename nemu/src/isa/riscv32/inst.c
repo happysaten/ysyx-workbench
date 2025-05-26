7fafa8d61b3bc93c70bc17b451bd6f19514677e4
tracer-ysyx <tracer@ysyx.org>
2025-05-26 21:09:40 +0800

    > compile NEMU ysyx_22040000 李丹青 Linux DESKTOP-BOOBDOU 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 GNU/Linux  21:09:40 up  3:22,  1 user,  load average: 0.13, 0.05, 0.01

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -138,6 +138,9 @@ static int decode_exec(Decode *s) {
     // xor: rd = src1 ^ src2，按位异或
     INSTPAT("0000000 ????? ????? 100 ????? 01100 11", xor, R,
             R(rd) = src1 ^ src2);
+    // or: rd = src1 | src2，按位或
+    INSTPAT("0000000 ????? ????? 110 ????? 01100 11", or, R,
+            R(rd) = src1 | src2);
     // ebreak: 触发调试断点或系统调用
     INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak, N,
             NEMUTRAP(s->pc, R(10))); // R(10) is $a0
