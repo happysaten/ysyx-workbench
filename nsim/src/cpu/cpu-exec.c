526ec32909e3bccad7ca045e0e31c9696a1204bd
tracer-ysyx <tracer@ysyx.org>
2025-07-20 19:51:38 +0800

    > compile NSIM ysyx_25050142 李丹青 Linux DESKTOP-BOOBDOU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  19:51:38 up  3:08,  1 user,  load average: 0.00, 0.09, 0.14

--- nsim/src/cpu/cpu-exec.c
+++ nsim/src/cpu/cpu-exec.c
@@ -88,7 +88,8 @@ static void exec_once(Decode *s, vaddr_t pc) {
 
     void disassemble(char *str, int size, uint64_t pc, uint8_t *code,
                      int nbyte);
-    printf("%s\n", s->logbuf);
+    printf("ilen = %d, pc = " FMT_WORD ", snpc = " FMT_WORD "\n",
+           ilen, s->pc, s->snpc);
     disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
                 MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst,
                 ilen);
