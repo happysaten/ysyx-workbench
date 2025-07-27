9fa2c3b25fb92e89cbddac9ef873f4d1ad2a12cf
tracer-ysyx <tracer@ysyx.org>
2025-07-27 19:28:28 +0800

    > compile NSIM ysyx_25050142 李丹青 Linux DESKTOP-BOOBDOU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  19:28:28 up  2:25,  1 user,  load average: 0.06, 0.12, 0.18

--- abstract-machine/scripts/platform/nsim.mk
+++ abstract-machine/scripts/platform/nsim.mk
@@ -16,6 +16,7 @@ NEMUFLAGS ?=
 NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nsim-log.txt
 # NEMUFLAGS += -b
 NEMUFLAGS += -e $(IMAGE).elf
+$(info NEMUFLAGS = $(NEMUFLAGS))
 
 MAINARGS_MAX_LEN = 64
 MAINARGS_PLACEHOLDER = The insert-arg rule in Makefile will insert mainargs here.
