5a138950a78d6a1cc1d0cd604566214201e7e4e4
tracer-ysyx <tracer@ysyx.org>
2025-08-31 12:08:41 +0800

    > compile NSIM ysyx_25050142 李丹青 Linux DESKTOP-BOOBDOU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  12:08:41 up 32 min,  1 user,  load average: 0.12, 0.17, 0.12

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,18 +1,20 @@
 #include <am.h>
 #include <nemu.h>
+#include <sys/types.h>
 
-void __am_timer_init() {
-}
+void __am_timer_init() {}
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+    uint64_t us_1 = inl(RTC_ADDR + 4);
+    uint64_t us_0 = inl(RTC_ADDR);
+    uptime->us = (us_1 << 32) | us_0;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
-  rtc->second = 0;
-  rtc->minute = 0;
-  rtc->hour   = 0;
-  rtc->day    = 0;
-  rtc->month  = 0;
-  rtc->year   = 1900;
+    rtc->second = 0;
+    rtc->minute = 0;
+    rtc->hour = 0;
+    rtc->day = 0;
+    rtc->month = 0;
+    rtc->year = 1900;
 }
