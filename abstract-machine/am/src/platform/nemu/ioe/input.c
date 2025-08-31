f96a2eed148fbb181cd1f8d25565871a144742fa
tracer-ysyx <tracer@ysyx.org>
2025-08-31 18:10:23 +0800

    > compile NSIM ysyx_25050142 李丹青 Linux DESKTOP-BOOBDOU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  18:10:23 up  1:08,  1 user,  load average: 1.24, 1.43, 1.03

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,10 +4,19 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  // 从键盘地址读取16位键盘码
-  uint32_t code = inw(KBD_ADDR);
-  // 判断是否按下
-  kbd->keydown = (code & KEYDOWN_MASK) ? 1 : 0;
-  // 设置按键码, 如果未按下则设置为AM_KEY_NONE
-  kbd->keycode = kbd->keydown ? (code & ~KEYDOWN_MASK) : AM_KEY_NONE;
+    // 从键盘地址读取16位键盘码
+    uint32_t code = inw(KBD_ADDR);
+    // // 判断是否按下
+    // kbd->keydown = (code & KEYDOWN_MASK) ? 1 : 0;
+    // // 设置按键码, 如果未按下则设置为AM_KEY_NONE
+    // kbd->keycode = kbd->keydown ? (code & ~KEYDOWN_MASK) : AM_KEY_NONE;
+    if (code != 0) {
+        // 提取键码（低15位）和按下状态（高位）
+        kbd->keycode = code & ~KEYDOWN_MASK;
+        kbd->keydown = (code & KEYDOWN_MASK) ? 1 : 0;
+    } else {
+        // 无按键时，返回AM_KEY_NONE
+        kbd->keydown = 0;
+        kbd->keycode = AM_KEY_NONE;
+    }
 }
