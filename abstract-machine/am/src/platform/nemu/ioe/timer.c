4b8ac9246d9e26d294763dbe4adfcb676e1570fb
tracer-ysyx <tracer@ysyx.org>
2025-08-31 12:18:01 +0800

    > compile NSIM ysyx_25050142 李丹青 Linux DESKTOP-BOOBDOU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  12:18:01 up 42 min,  1 user,  load average: 0.01, 0.12, 0.13

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -4,12 +4,14 @@
 
 void __am_timer_init() {}
 
+// AM系统启动时间, 可读出系统启动后的微秒数.
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
     uint64_t us_1 = inl(RTC_ADDR + 4);
     uint64_t us_0 = inl(RTC_ADDR);
     uptime->us = (us_1 << 32) | us_0;
 }
 
+// AM实时时钟(RTC, Real Time Clock), 可读出当前的年月日时分秒
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
     rtc->second = 0;
     rtc->minute = 0;
