d03e98dd03608041ca3a34418bba82e837d5562b
tracer-ysyx <tracer@ysyx.org>
2025-09-01 11:33:45 +0800

    > compile NSIM ysyx_25050142 李丹青 Linux DESKTOP-BOOBDOU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  11:33:45 up  1:00,  1 user,  load average: 0.22, 0.26, 0.26

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -31,7 +31,7 @@ void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
     // 设置每次播放的样本数量
     outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
     // 初始化音频设备
-    __am_audio_init();
+    // __am_audio_init();
 }
 
 // 获取音频设备状态
