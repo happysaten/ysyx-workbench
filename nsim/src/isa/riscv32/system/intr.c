5171776a5bb5306a46f8e1bc1fde1290c51f8bfc
tracer-ysyx <tracer@ysyx.org>
2025-09-02 14:43:27 +0800

    > compile NSIM ysyx_25050142 李丹青 Linux DESKTOP-BOOBDOU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  14:43:27 up  4:28,  1 user,  load average: 0.08, 0.14, 0.16

--- nsim/src/isa/riscv32/system/intr.c
+++ nsim/src/isa/riscv32/system/intr.c
@@ -1,20 +1,20 @@
 /***************************************************************************************
-* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
-*
-* NSIM is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+ *
+ * NSIM is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
-#include <isa.h>
 #include "../local-include/reg.h"
+#include <isa.h>
 
 /**
  * 触发异常/中断处理
@@ -23,26 +23,27 @@
  * @return 异常/中断向量地址 - 异常处理程序的入口地址
  */
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
-  /* 实现RISC-V异常处理机制:
-   * 1. 保存触发异常时的PC到mepc寄存器
-   * 2. 设置异常原因到mcause寄存器  
-   * 3. 更新mstatus寄存器的状态位
-   * 4. 返回异常处理程序入口地址(mtvec寄存器值)
-   */
-  
-  // 保存异常发生时的PC值到mepc寄存器
-  csr(CSR_MEPC) = epc;  // MEPC
-  
-  // 设置异常原因码到mcause寄存器
-  // 最高位为0表示异常，为1表示中断
-  csr(CSR_MCAUSE) = NO;  // MCAUSE
-  
-  // 更新mstatus寄存器
-  // 在实际实现中，这里应该保存和更新中断使能位等状态
-  // 简化实现：保持当前mstatus值不变
-  
-  // 返回异常向量地址，异常处理程序将从此地址开始执行
-  return csr(CSR_MTVEC);  // MTVEC
+    /* 实现RISC-V异常处理机制:
+     * 1. 保存触发异常时的PC到mepc寄存器
+     * 2. 设置异常原因到mcause寄存器
+     * 3. 更新mstatus寄存器的状态位
+     * 4. 返回异常处理程序入口地址(mtvec寄存器值)
+     */
+
+    // 保存异常发生时的PC值到mepc寄存器
+    csr(CSR_MEPC) = epc; // MEPC
+
+    // 设置异常原因码到mcause寄存器
+    // 最高位为0表示异常，为1表示中断
+    csr(CSR_MCAUSE) = NO; // MCAUSE
+
+    // 更新mstatus寄存器
+    // 在实际实现中，这里应该保存和更新中断使能位等状态
+    // 简化实现：保持当前mstatus值不变
+    log_write("[etrace] ecall at pc = " FMT_WORD " cause = " FMT_WORD "\n",
+              csr(CSR_MEPC), csr(CSR_MCAUSE));
+    // 返回异常向量地址，异常处理程序将从此地址开始执行
+    return csr(CSR_MTVEC); // MTVEC
 }
 
 /**
@@ -50,7 +51,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
  * @return 中断状态 - 当前返回INTR_EMPTY表示无待处理中断
  */
 word_t isa_query_intr() {
-  // 目前返回无中断状态
-  // 在完整实现中，这里应该检查中断控制器和中断使能位
-  return INTR_EMPTY;
+    // 目前返回无中断状态
+    // 在完整实现中，这里应该检查中断控制器和中断使能位
+    return INTR_EMPTY;
 }
