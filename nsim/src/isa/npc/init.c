c397067815aee635533779e4971d8dd422c27117
tracer-ysyx <tracer@ysyx.org>
2025-09-09 21:31:01 +0800

    > sim RTL ysyx_25050142 李丹青 Linux DESKTOP-BOOBDOU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  21:31:01 up  4:29,  1 user,  load average: 0.25, 0.19, 0.13

--- nsim/src/isa/npc/init.c
+++ nsim/src/isa/npc/init.c
@@ -1,17 +1,17 @@
 /***************************************************************************************
-* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
-*
-* NSIM is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+ *
+ * NSIM is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include <isa.h>
 #include <memory/paddr.h>
@@ -19,16 +19,16 @@
 
 // img数组为内置指令镜像，直接以uint32_t存储指令数据
 // 注意：与uint8_t不一致，但不会直接访问数组内容，因此无影响
-static const uint32_t img [] = {
-  0x00000297,  // auipc t0,0   // 将当前PC存入t0寄存器
-  0x00028823,  // sb  zero,16(t0) // 将0写入t0+16地址
-  0x0102c503,  // lbu a0,16(t0)  // 从t0+16地址读入a0
-  0x00100073,  // ebreak (used as nsim_trap) // 触发断点（模拟trap）
-  0xdeadbeef,  // some data // 测试数据
-  // t0 = PC + (0 << 12) = 0x80000000
-  // 将0写入地址 t0+0x10 = 0x80000010
-  // 从地址 t0+0x10 = 0x80000010 读取字节到a0
-  // 触发断点异常
+static const uint32_t img[] = {
+    0x00000297, // auipc t0,0   // 将当前PC存入t0寄存器
+    0x00028823, // sb  zero,16(t0) // 将0写入t0+16地址
+    0x0102c503, // lbu a0,16(t0)  // 从t0+16地址读入a0
+    0x00100073, // ebreak (used as nsim_trap) // 触发断点（模拟trap）
+    0xdeadbeef, // some data // 测试数据
+                // t0 = PC + (0 << 12) = 0x80000000
+                // 将0写入地址 t0+0x10 = 0x80000010
+                // 从地址 t0+0x10 = 0x80000010 读取字节到a0
+                // 触发断点异常
 };
 
 // 声明复位CPU的函数
@@ -36,18 +36,21 @@ void reset_cpu();
 
 // restart函数用于重置CPU状态并设置初始PC
 static void restart() {
-  reset_cpu(); // 复位CPU所有寄存器和状态
-  /* 设置初始程序计数器（PC） */
-  cpu.pc = RESET_VECTOR;
-  /* 零号寄存器始终为0 */
-  cpu.gpr[0] = 0;
+    reset_cpu(); // 复位CPU所有寄存器和状态
+    /* 设置初始程序计数器（PC） */
+    cpu.pc = RESET_VECTOR;
+    /* 零号寄存器始终为0 */
+    cpu.gpr[0] = 0;
+
+    /* Initialize mstatus register. */
+    cpu.mstatus = MUXDEF(CONFIG_RV64, 0xa00001800, 0x1800);
 }
 
 // init_isa函数用于初始化ISA相关内容
 void init_isa() {
-  /* 加载内置镜像到内存 */
-  memcpy(guest_to_host(RESET_VECTOR), img, sizeof(img));
+    /* 加载内置镜像到内存 */
+    memcpy(guest_to_host(RESET_VECTOR), img, sizeof(img));
 
-  /* 初始化虚拟计算机系统 */
-  restart();
+    /* 初始化虚拟计算机系统 */
+    restart();
 }
