e006113eff801e1be2b06a897a4401f9473c4171
tracer-ysyx <tracer@ysyx.org>
2025-10-17 19:04:34 +0800

    > sim RTL ysyx_25050142 李丹青 Linux DESKTOP-BOOBDOU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  19:04:34 up  3:34,  1 user,  load average: 0.38, 0.30, 0.30

--- nsim/src/verilator/exec.cpp
+++ nsim/src/verilator/exec.cpp
@@ -46,7 +46,7 @@ static void step() {
 // 执行单条CPU指令
 extern "C" void exec_one_cpu() {
     // top->inst = paddr_read(top->pc, 4); // 从PC地址获取指令
-    for (int i = 0; i < 1; i++) {
+    for (int i = 0; i < 2; i++) {
         step(); // 仿真两个周期以完成指令执行
     }
     // int count = 0;
