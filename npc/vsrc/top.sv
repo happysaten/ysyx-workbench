1b4d3062c1c10110045fd5d5da25cb8e539cd57c
tracer-ysyx <tracer@ysyx.org>
2025-10-18 18:31:40 +0800

    > sim RTL ysyx_25050142 李丹青 Linux DESKTOP-BOOBDOU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  18:31:40 up  2:47,  1 user,  load average: 0.18, 0.40, 0.53

--- npc/vsrc/top.sv
+++ npc/vsrc/top.sv
@@ -224,7 +224,7 @@ module IFU (
     // );
     // assign ifu_req_valid_q = lfsr8_out && state == WAIT;
     delay_line #(
-        .N(1),
+        .N(2),
         .WIDTH(1)
     ) u_delay_line (
         .clk  (clk),
