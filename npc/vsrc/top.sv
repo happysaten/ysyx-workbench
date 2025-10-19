90a7f16981a5896af2a4e1827c63938551cb501f
tracer-ysyx <tracer@ysyx.org>
2025-10-19 14:35:52 +0800

    > sim RTL ysyx_25050142 李丹青 Linux DESKTOP-BOOBDOU 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  14:35:52 up  4:06,  1 user,  load average: 0.16, 0.10, 0.09

--- npc/vsrc/top.sv
+++ npc/vsrc/top.sv
@@ -754,10 +754,10 @@ module LSU (
     logic pmem_ren, pmem_wen, pmem_req;
     assign pmem_req = pmem_ren || pmem_wen;
     always @(posedge clk) begin
-        if (pmem_ren && next_state == RESP) pmem_rdata <= pmem_read_npc(pmem_addr);
+        if (pmem_ren && req_fire && resp_data_ready) pmem_rdata <= pmem_read_npc(pmem_addr);
     end
     always_comb
-        if (pmem_wen && next_state == RESP) pmem_write_npc(pmem_addr, pmem_wdata, pmem_wmask);
+        if (pmem_wen && req_fire && resp_data_ready) pmem_write_npc(pmem_addr, pmem_wdata, pmem_wmask);
 
     // 指令逻辑
     assign pmem_ren   = (inst_type == TYPE_I && opcode == 7'b0000011);
